f25a306be1cc0c39c7d8dde805ebb3b22b5e5a05
Daniel Jimenez <danyjimenez789@gmail.com>
2024-12-09 22:39:51 -0600

    componente capitulos

---
+++ src/app/pages/capitulos/capitulos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-capitulos',
+  standalone: true,
+  imports: [],
+  templateUrl: './capitulos.component.html',
+  styleUrl: './capitulos.component.css'
+})
+export class CapitulosComponent {
+
+}
