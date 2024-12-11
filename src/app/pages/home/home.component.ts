83b27e30fcc9528a32af05b80f97bddc0ed9cdde
Daniel Jimenez <danyjimenez789@gmail.com>
2024-12-10 22:38:20 -0600

    login logic

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  email:string = '';
+  password:string = '';
+
+  constructor(private router: Router) {}
+
+  onLogin(){
+    if(this.email === 'daniel.jimenez1439@alumnos.udg.mx' && this.password === '123'){
+      this.router.navigate(['dashboard']);
+    }else{
+      alert('Usuario No Valido!');
+    }
+  }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
