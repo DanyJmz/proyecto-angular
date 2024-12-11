810b3d6499e55995ebf0f43100bb5756c4b57d4f
Daniel Jimenez <danyjimenez789@gmail.com>
2024-12-10 22:36:49 -0600

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { PersonajesComponent } from './pages/personajes/personajes.component';
+import { CapitulosComponent } from './pages/capitulos/capitulos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"",
+        component: HomeComponent
+    },
+    {
+        path:"dashboard",
+        component: DashboardComponent
+    },
+    {
+        path:"personajes",
+        component: PersonajesComponent
+    },
+    {
+        path:"capitulos",
+        component:CapitulosComponent
+    }
+];
+
+
