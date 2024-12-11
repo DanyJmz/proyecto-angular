9577b1c40feaf024886eb172cfe2db3cd5a734d9
Daniel Jimenez <danyjimenez789@gmail.com>
2024-12-10 22:38:35 -0600

    capitulos

--- src/app/pages/capitulos/capitulos.component.ts
+++ src/app/pages/capitulos/capitulos.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { CardComponent } from '../../components/card/card.component';
 @Component({
   selector: 'app-capitulos',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './capitulos.component.html',
   styleUrl: './capitulos.component.css'
 })
